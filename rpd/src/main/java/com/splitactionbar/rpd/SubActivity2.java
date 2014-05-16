55c2a2144245e30e3b9d8fec04112240e51efe4e
Dorian <i.am.eager.for.peace@gmail.com>
2014-05-16 17:34:57 +0900

    modified activity

--- rpd/src/main/java/com/splitactionbar/rpd/SubActivity2.java
+++ rpd/src/main/java/com/splitactionbar/rpd/SubActivity2.java
@@ -1,7 +1,15 @@
 package com.splitactionbar.rpd;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by rpd on 14/05/16.
  */
-public class SubActivity2 {
+public class SubActivity2 extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
