dc527c90a654c44d943a773a3c8b9b20fb91bb03
Dorian <i.am.eager.for.peace@gmail.com>
2014-05-16 18:05:05 +0900

    よくわからない挙動をしたので、いったんfeatureを切って検証

--- rpd/src/main/java/com/splitactionbar/rpd/SubActivity2.java
+++ rpd/src/main/java/com/splitactionbar/rpd/SubActivity2.java
@@ -11,5 +11,6 @@ public class SubActivity2 extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub2);
     }
 }
